38d1b5b1de26d9bd71ab01ed52f95896eb5371b6
fbonifac <f.t.bonifac@gmail.com>
2025-11-18 13:55:51 +0100

    Code for N trajectories added

---
+++ Eval/EvaluateModel.py
@@ -0,0 +1,177 @@
+#Packages:
+import torch
+from torch import nn
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+#Own code:
+from Pendulum.pendulum import N_PendulumSystem
+from KKL.KKL import N_KKLSystem
+from REN.REN import N_RENSystem, N_RENSystem_TORCHODE
+
+def evaluate_model(T_test, test_function, experiment_folder='../Data/Experiment12/', plot=False):
+
+    hyperparameters = np.load(experiment_folder + 'hyperparameters.npy', allow_pickle=True).item()
+
+    nx = hyperparameters['nx']
+    nq = hyperparameters['nq']
+    nu = hyperparameters['nu']
+    ny = hyperparameters['ny']
+    nkkl = hyperparameters['nkkl']
+    kkl_poles = hyperparameters['KKL_poles']
+    bias = hyperparameters['bias']
+    sigma = hyperparameters['activation_function']
+    device = hyperparameters['device']
+    dT = hyperparameters['sampling_time']
+    N = hyperparameters['number_of_experiments']
+    default_type = hyperparameters['variable_type']
+    T_trans = hyperparameters['transient_time']
+
+    integration_method = hyperparameters['integration_method']
+    integration_steps = hyperparameters['fixed_number_of_steps']
+    integration_tol = hyperparameters['integration_tolerance']
+    integration_atol = hyperparameters['integration_absolute_tolerance']
+
+    torch.set_default_dtype(default_type)
+
+    training = torch.load(experiment_folder + 'model_and_optimizer.pt')
+
+
+    """Initialize system that will be observed:
+        -Initial states at: rand([1,1],[-1,-1])
+        -Parameters of the system:
+            -beta: 1.5
+            -l: 0.5
+            -g: 9.81
+    """
+    initial_pendulum_states = np.random.uniform(-1, 1, (N, 2))
+    pendulum = N_PendulumSystem(initial_pendulum_states, 1.5, 0.5, 9.81)
+    #Vales fixed to the pendulum
+    nxsys = pendulum.nx
+    nysys = pendulum.ny
+    nusys = pendulum.nu
+
+    """Initialize KKL system (z transformed system):
+        -Initial states at: zeros((N, nkkl))
+        -Parameters of the system:
+            -A: diag(kkl_poles)
+            -B: ones((nkkl, nusys))
+    """
+    #Load KKL poles:
+    A = np.diag(kkl_poles)
+    B = np.ones((nkkl, nysys))
+    initial_KKL_states = np.zeros((N, nkkl), dtype=np.float32)
+    kkl = N_KKLSystem(initial_KKL_states, A, B)
+
+    """Initialize REN (Tau* transformation from (u, y, z) to (x)):
+        -Parameters of the system:
+            -nx: nx
+            -nq: nq
+            -nu: nu
+            -ny: ny
+    """
+    model = N_RENSystem_TORCHODE(nx, nq, nu, ny, bias=bias, sigma=sigma, device=device)
+    model.load_state_dict(training['model'])
+    model.updateParameters()
+
+    loss = 1.0
+    MSE = nn.MSELoss()
+
+    N_trans = int(T_trans / dT) + 1
+    N_test = int(T_test / dT) + 1
+
+    # ---------------------------------------TRANSIENT PHASE------------------------------------------------
+    # The whole system (Pendulum+KKL+REN) is simulated for T_trans to line up
+
+    # Create time vector and input vector for transient phase
+    t_trans = np.linspace(0, T_trans, N_trans)
+    u_trans = np.zeros((N, nusys, N_trans))
+
+    # Simulate the pendulum, calculate output, simulate the KKL system, create the input for the REN, simulate the REN and save the last state
+    with torch.no_grad():
+        x_trans = pendulum.simulate(t_trans, u_trans)
+        y_trans = pendulum.output(x_trans)
+        z_trans = kkl.simulate(t_trans, y_trans)
+        u_y_trans = np.append(u_trans, y_trans, axis=1)
+        ren_u_trans = np.append(u_y_trans, z_trans, axis=1)
+
+        t_trans_torch = torch.from_numpy(t_trans).float()
+        t_trans_torch = t_trans_torch.to(device)
+        ren_u_trans_torch = torch.from_numpy(ren_u_trans).float()
+        ren_u_trans_torch = ren_u_trans_torch.to(device)
+
+        ren_x_init = torch.zeros(N, nx, device=device)
+        ren_y_trans = model.simulate(t_trans_torch, ren_u_trans_torch, ren_x_init,
+                                     integration_method=integration_method, integration_steps=integration_steps,
+                                     integration_tol=integration_tol, integration_atol=integration_atol)
+
+    if plot:
+        plt.plot(t_trans, x_trans[1, -1, :], linewidth=1.5, label=r'$x_2(t)$')
+        plt.plot(t_trans, ren_y_trans[1, 0, :].detach().cpu(), linewidth=1.5, label=r'$\hat{x}_2(t)$')
+        plt.xlabel(r'$time [s]$')
+        plt.ylabel(r'$x_2(t) [rad/s]$')
+        plt.legend(loc='best')
+        plt.title('Transient trajectories of the last experiment')
+        plt.show()
+
+    # ---------------------------------------TESTING PHASE------------------------------------------------
+    # The whole system (Pendulum+KKL+REN) is simulated for T_test with input test_function to test the experiment
+
+    # Create time vector and input vector for testing phase
+    t_test = np.linspace(0, T_test, N_test)
+    u_test = np.zeros((N, nusys, N_test))
+
+    for i in range(N):
+        u_test[i,:,:] = test_function(t_test)
+
+    with torch.no_grad():
+        x_test = pendulum.simulate(t_test, u_test)
+        y_test = pendulum.output(x_test)
+        z_test = kkl.simulate(t_test, y_test)
+        u_y_test = np.append(u_test, y_test, axis=1)
+        ren_u_test = np.append(u_y_test, z_test, axis=1)
+
+        t_test_torch = torch.from_numpy(t_test).float()
+        t_test_torch = t_test_torch.to(device)
+        ren_u_test_torch = torch.from_numpy(ren_u_test).float()
+        ren_u_test_torch = ren_u_test_torch.to(device)
+
+        y_predict_test = x_test[:, nysys:, :]
+        y_predict_test_torch = torch.from_numpy(y_predict_test).float()
+        y_predict_test_torch = y_predict_test_torch.to(device)
+
+        ren_y_test = model.simulate(t_test_torch, ren_u_test_torch, ren_x_init,
+                                     integration_method=integration_method, integration_steps=integration_steps,
+                                     integration_tol=integration_tol, integration_atol=integration_atol)
+
+        loss = MSE(ren_y_test, y_predict_test_torch)
+        print(loss)
+        nrmse_base = MSE(torch.zeros_like(y_predict_test_torch), y_predict_test_torch)
+        nrmse = torch.sqrt(loss/nrmse_base)
+        print(nrmse)
+
+    if plot:
+        M = 3
+        rindex = np.random.randint(0, N, M*M)
+
+        for i in range(M):
+            for j in range(M):
+                plt.subplot(M, M, i*M+j+1)
+                plt.plot(t_test, y_predict_test_torch[rindex[i*M+j], 0, :].detach().cpu(), linewidth=1.5, label=r'$x_2(t)$')
+                plt.plot(t_test, ren_y_test[rindex[i*M+j], 0, :].detach().cpu(), linewidth=1.5, label=r'$\hat{x}_2(t)$')
+
+
+        plt.show()
+
+
+
+def utest(t):
+    n = t.shape[0]
+    u = np.ones((1,n))
+    step = np.random.rand()
+    u[:,:int(n*step)] = (np.random.rand()-0.5)*10*u[:,:int(n*step)]
+    u[:,int(n*step):] = (np.random.rand()-0.5)*10*u[:,int(n*step):]
+    return u
+
+evaluate_model(8, utest, experiment_folder='../Data/Experiment1/', plot=True)
--- REN/REN.py
+++ REN/REN.py
@@ -1,14 +1,15 @@
 #Packages:
-import numpy as np
 import torch
 import torch.nn.functional as F
+from sympy.codegen.fnodes import intent_in
 from torch import nn
-from torchdiffeq import odeint_adjoint as odeint
+from torchdiffeq import odeint, odeint_adjoint
+import torchode as to
 
 class REN(nn.Module):
     def __init__(self, nx, nq, nu, ny, sigma = 'relu', epsilon = 1.0e-2, device = 'cuda', bias = False):
         """
-        Class of Reccurent Equilibrium Networks.
+        Class of Recurrent Equilibrium Networks.
 
         Args:
             -nx : int - number of internal-states
@@ -16,7 +17,7 @@ class REN(nn.Module):
             -nu : int - number of inputs
             -ny : int - number of outputs
             -sigma : string - activation function sigma(), default: tanh
-            -epsilon : float - small positive number to ensure the matrices are positve-definite, default: 1e-2
+            -epsilon : float - small positive number to ensure the matrices are positive-definite, default: 1e-2
             -device : string - device to be used for the computations using Pytorch
             -bias : bool - whether to use bias or not, default: False
         """
@@ -86,9 +87,9 @@ class REN(nn.Module):
         H = F.linear(self.X, self.X) + self.epsilon*torch.eye(self.nx + self.nq, self.nx + self.nq, device=self.device)
 
         #Partition H into H11, H12, H21, H22
-        h1, h2 = torch.split(H, (self.nx, self.nq), dim=0)
-        H11, H12 = torch.split(h1, (self.nx, self.nq), dim=1)
-        H21, H22 = torch.split(h2, (self.nx, self.nq), dim=1)
+        h1, h2 = torch.split(H, [self.nx, self.nq], dim=0)
+        H11, H12 = torch.split(h1, [self.nx, self.nq], dim=1)
+        H21, H22 = torch.split(h2, [self.nx, self.nq], dim=1)
 
         #Calculate A, D11, C1, B1
         Y = -0.5*(H11 + self.Y1 - self.Y1.T)
@@ -156,27 +157,27 @@ class RENSystem(nn.Module):
         y = self.sys.output(t, x, self.u)
         return y
 
-    def simulate(self, timevector, inputvector, initial_condition=None):
+    def simulate(self, time_vector, input_vector, initial_condition=None):
         """Given inputs over times in the time vector the function simulates the system and returns the output
-        over the given timevector."""
+        over the given time vector."""
 
         if initial_condition is not None:
             self.x = initial_condition
 
-        inputvector_output = torch.matmul(inputvector.T, self.input_output_matrix).T
+        input_vector_output = torch.matmul(input_vector.T, self.input_output_matrix).T
 
-        N = timevector.shape[0]
+        N = time_vector.shape[0]
         output = torch.zeros(self.sys.ny, N, device=self.sys.device)
-        self.u = inputvector_output[:, 0]
-        output[:, 0] = self.output(timevector[0], self.x)
+        self.u = input_vector_output[:, 0]
+        output[:, 0] = self.output(time_vector[0], self.x)
 
         for t in range(1, N):
-            timestep = timevector[t-1:t+1]
-            self.u = inputvector[:, t] #Zero-order hold?
+            timestep = time_vector[t-1:t+1]
+            self.u = input_vector[:, t] #Zero-order hold?
             temp_x = odeint(self, self.x, timestep, method='dopri5', rtol=1.0e-3, atol=1.0e-5, adjoint_rtol=1.0e-3, adjoint_atol=1.0e-5)
             self.x = temp_x[1, :]
-            self.u = inputvector_output[:, t]
-            output[:, t] = self.output(timevector[t], self.x)
+            self.u = input_vector_output[:, t]
+            output[:, t] = self.output(time_vector[t], self.x)
         return output
 
     def nfe(self):
@@ -187,7 +188,7 @@ class RENSystem(nn.Module):
 class N_REN(nn.Module):
     def __init__(self, nx, nq, nu, ny, sigma = 'relu', epsilon = 1.0e-2, device = 'cuda', bias = False):
         """
-        Class of Reccurent Equilibrium Networks.
+        Class of Recurrent Equilibrium Networks.
 
         Args:
             -nx : int - number of internal-states
@@ -195,7 +196,7 @@ class N_REN(nn.Module):
             -nu : int - number of inputs
             -ny : int - number of outputs
             -sigma : string - activation function sigma(), default: tanh
-            -epsilon : float - small positive number to ensure the matrices are positve-definite, default: 1e-2
+            -epsilon : float - small positive number to ensure the matrices are positive-definite, default: 1e-2
             -device : string - device to be used for the computations using Pytorch
             -bias : bool - whether to use bias or not, default: False
         """
@@ -220,18 +221,13 @@ class N_REN(nn.Module):
         self.C2 = nn.Parameter(torch.randn(ny, nx, requires_grad=True, device=device) * std)
         self.D12 = nn.Parameter(torch.randn(nq, nu, requires_grad=True, device=device) * std)
         self.D21 = nn.Parameter(torch.randn(ny, nq, requires_grad=True, device=device) * std)
-        D22yKLL = nn.Parameter(torch.randn(ny, nu-1, requires_grad=True, device=device) * std)
-        D22u = torch.zeros(ny, 1, device=device)
-        self.D22 = torch.cat((D22u, D22yKLL), dim=1)
+        self.D22yKLL = nn.Parameter(torch.randn(ny, nu-1, requires_grad=True, device=device) * std)
 
         # Initialization of the Biases:
         if bias:
             self.bx = nn.Parameter(torch.zeros(nx, requires_grad=True, device=device))
             self.bv = nn.Parameter(torch.zeros(nq, requires_grad=True, device=device))
             self.by = nn.Parameter(torch.zeros(ny, requires_grad=True, device=device))
-            #self.bx = nn.Parameter(torch.randn(nx, requires_grad=True, device=device)*std)
-            #self.bv = nn.Parameter(torch.randn(nq, requires_grad=True, device=device)*std)
-            #self.by = nn.Parameter(torch.randn(ny, requires_grad=True, device=device)*std)
         else:
             self.bx = torch.zeros(nx, device=device)
             self.bv = torch.zeros(nq, device=device)
@@ -243,6 +239,7 @@ class N_REN(nn.Module):
         self.C1 = torch.zeros(nq, nx, device=device)
         self.B1 = torch.zeros(nx, nq, device=device)
         self.P = torch.zeros(nx, nx, device=device)
+        self.D22 = torch.zeros(ny, nu, device=device)
 
         self.updateParameters()             #Update of: A, B1, C1, D11, D12, D22
 
@@ -268,9 +265,9 @@ class N_REN(nn.Module):
         H = F.linear(self.X, self.X) + self.epsilon*torch.eye(self.nx + self.nq, self.nx + self.nq, device=self.device)
 
         #Partition H into H11, H12, H21, H22
-        h1, h2 = torch.split(H, (self.nx, self.nq), dim=0)
-        H11, H12 = torch.split(h1, (self.nx, self.nq), dim=1)
-        H21, H22 = torch.split(h2, (self.nx, self.nq), dim=1)
+        h1, h2 = torch.split(H, [self.nx, self.nq], dim=0)
+        H11, H12 = torch.split(h1, [self.nx, self.nq], dim=1)
+        H21, H22 = torch.split(h2, [self.nx, self.nq], dim=1)
 
         #Calculate A, D11, C1, B1
         Y = -0.5*(H11 + self.Y1 - self.Y1.T)
@@ -281,8 +278,12 @@ class N_REN(nn.Module):
         Z = -H12 - self.U
         self.B1 = F.linear(torch.inverse(P), Z.T)
 
+        D22u = torch.zeros(self.ny, 1, device=self.device)
+        self.D22 = torch.cat((D22u, self.D22yKLL), dim=1)
+
+        """
         #Check if the LMI holds:
-        """with torch.no_grad():
+        with torch.no_grad():
             temp1 = torch.cat((-F.linear(self.A.T, P.T) - F.linear(P.T, self.A.T), -F.linear(self.C1.T, Lambda.T) - F.linear(P, self.B1.T)), 1)
             temp2 = torch.cat(((-F.linear(self.C1.T, Lambda.T) - F.linear(P, self.B1.T)).T, 2*Lambda - F.linear(Lambda, self.D11.T) - F.linear(self.D11.T, Lambda.T)), 1)
             check_matrix = torch.cat((temp1, temp2), 0)
@@ -293,6 +294,7 @@ class N_REN(nn.Module):
         """Given x(t), u(t) calculates w(t). (Solution of w(t) = sigma(C1*x(t) + D11*w(t) + D12*u(t) + b_v))
         This calculation exploits the fact that the matrix D11 is a lower triangular matrix."""
 
+        """
         N = x.shape[0]
         w = torch.zeros(N, self.nq, device=self.device)
         # Calculate the rest of w(t)
@@ -301,6 +303,25 @@ class N_REN(nn.Module):
             select_w[i,0] = 1.
             v = (F.linear(x, self.C1[i, :]) + F.linear(w, self.D11[i, :]) + F.linear(u, self.D12[i, :]) + self.bv[i]).unsqueeze(1)
             w = w + F.linear(self.act(v), select_w)
+        return w"""
+
+        N = x.shape[0]
+
+        base = (F.linear(x, self.C1) + F.linear(u, self.D12) + self.bv)
+
+        #w = torch.zeros(N, self.nq, device=self.device)
+        ws = []
+
+        for i in range(self.nq):
+            if i > 0:
+                prev_w = torch.stack(ws, dim=1)
+                dep = prev_w @ self.D11[i, :i].T
+            else:
+                dep = 0.0
+            wi = self.act(base[:,i] + dep)
+            ws.append(wi)
+
+        w = torch.stack(ws, dim=1)
         return w
 
     def forward(self, t, x, u):
@@ -316,13 +337,13 @@ class N_REN(nn.Module):
         return y
 
 class N_RENSystem(nn.Module):
-    def __init__(self, nx, nq, nu, ny, N, sigma='relu', epsilon=1.0e-2, device='cuda', bias=False):
+    def __init__(self, nx, nq, nu, ny, sigma='relu', epsilon=1.0e-2, device='cuda', bias=False):
 
         super().__init__()
         self.sys = N_REN(nx, nq, nu, ny, sigma, epsilon, device, bias)
         self.nfe = 0
-        self.x = torch.zeros(N, nx, device=device)
-        self.u = torch.zeros(N, nu, device=device)
+        self.u = None
+        self.x = None
 
     def updateParameters(self):
         self.sys.updateParameters()
@@ -337,32 +358,122 @@ class N_RENSystem(nn.Module):
         y = self.sys.output(t, x, self.u)
         return y
 
-    def simulate(self, timevector, inputvector, initial_condition=None):
+    def simulate(self, time_vector, input_vector, initial_condition, integration_method='dopri5', integration_steps=1):
         """Given inputs over times in the time vector the function simulates the system and returns the output
-        over the given timevector."""
+        over the given time vector."""
+
+        self.x = initial_condition
+
+        dt = (time_vector[1] - time_vector[0]) / integration_steps
+        ode_options = {'step_size' : dt.item()}
+
+        T = time_vector.shape[0]
+        N = self.x.shape[0]
+
+        input_vector_output = input_vector
+        input_vector_output[:, 0, :] = torch.zeros((N, T), device=self.sys.device)
+
+        output = torch.zeros(N, self.sys.ny, T, device=self.sys.device)
+        self.u = input_vector_output[:, :, 0]
+        output[:, :, 0] = self.output(time_vector[0], self.x)
+
+        for t in range(1, T):
+            timestep = time_vector[t-1:t+1]
+            self.u = input_vector[:, :, t] #Zero-order hold?
+            if integration_method=='dopri5':
+                temp_x = odeint(self, self.x, timestep, method='dopri5', atol=1.0e-5, rtol=1.0e-3, adjoint_atol=1.0e-5,
+                               adjoint_rtol=1.0e-3)
+            elif integration_method=='euler':
+                temp_x = odeint(self, self.x, timestep, method='euler', options=ode_options)
+            elif integration_method=='rk4':
+                temp_x = odeint(self, self.x, timestep, method='rk4',  options=ode_options)
+            else:
+                print(f"NO SUCH INTEGRATION METHOD: {integration_method}")
+                return 0
+
+            self.x = temp_x[1, :, :]
+            self.u = input_vector_output[:, :, t]
+            output[:, :, t] = self.output(time_vector[t], self.x)
+        return output
+
+    def nfe(self):
+        """Number of forward evaluations"""
+        return self.nfe
+
+class N_RENSystem_TORCHODE(nn.Module):
+    def __init__(self, nx, nq, nu, ny, sigma='relu', epsilon=1.0e-2, device='cuda', bias=False):
+
+        super().__init__()
+        self.sys = N_REN(nx, nq, nu, ny, sigma, epsilon, device, bias)
+        self.nfe = 0
+        self.u = None
+        self.x = None
+
+    def updateParameters(self):
+        self.sys.updateParameters()
+
+    def forward(self, t, x):
+        self.nfe += 1
+        x_dot = self.sys(t, x, self.u)
+        return x_dot
+
+    def output(self, t, x):
+        self.nfe += 1
+        y = self.sys.output(t, x, self.u)
+        return y
+
+    def simulate(self, time_vector, input_vector, initial_condition=None, integration_method='dopri5', integration_steps=10, integration_tol=1.0e-4, integration_atol=1.0e-6):
+        """Given inputs over times in the time vector the function simulates the system and returns the output
+        over the given time vector."""
 
         if initial_condition is not None:
             self.x = initial_condition
 
-        T = timevector.shape[0]
+        T = time_vector.shape[0]
         N = self.x.shape[0]
+        dT = time_vector[1] - time_vector[0]
 
-        inputvector_output = inputvector
-        inputvector_output[:, 0, :] = torch.zeros((N, T), device=self.sys.device)
+        time_vector = time_vector.repeat(N,1)
 
+        def dynamics(t, x):
+            temp = 1/(1 + F.relu(time_vector[0,:]- t[0]))
+            map_idx = torch.max(torch.flip(temp, [0]), dim=0)
+            t_idx = T-map_idx[1].item()-1
+            self.u = input_vector[:,:,t_idx]
+            x_dot = self.forward(t, x)
+            return x_dot
+
+        term = to.ODETerm(dynamics)
+
+        if integration_method == 'dopri5':
+            step_method = to.Dopri5(term=term)
+            controller = to.IntegralController(atol=integration_atol, rtol=integration_tol, term=term)
+        elif integration_method == 'rk4':
+            step_method = to.RK4(term=term)
+            controller = to.FixedStepController()
+        elif integration_method == 'euler':
+            step_method = to.Euler(term=term)
+            controller = to.FixedStepController()
+        else:
+            raise ValueError(f"Integration method {integration_method} not supported")
+
+        solver = to.AutoDiffAdjoint(step_method, controller)
+        #jit_solver = torch.compile(solver)
+
+        dt0 = torch.full((N,), dT/integration_steps, device=self.sys.device)
+        ivp = to.InitialValueProblem(y0=self.x, t_eval=time_vector)
+        sol = solver.solve(ivp, dt0=dt0)
+
+        ys = sol.ys
+        ys = ys.permute(0, 2, 1).contiguous()
+
+        input_vector_output = input_vector.clone()
+        input_vector_output[:, 0, :] = torch.zeros((N, T), device=self.sys.device)
         output = torch.zeros(N, self.sys.ny, T, device=self.sys.device)
-        self.u = inputvector_output[:, :, 0]
-        output[:, :, 0] = self.output(timevector[0], self.x)
+        for t in range(T):
+            self.u = input_vector_output[:, :, t]
+            output[:, :, t] = self.output(time_vector[0,t], ys[:, :, t])
 
-        for t in range(1, T):
-            timestep = timevector[t-1:t+1]
-            self.u = inputvector[:, :, t] #Zero-order hold?
-
-            #temp_x = odeint(self, self.x, timestep, method='euler', options=dict(stepsize=(timevector[1]-timevector[0])))
-            temp_x = odeint(self, self.x, timestep, method='dopri5', atol=1.0e-5, rtol=1.0e-3, adjoint_atol=1.0e-5, adjoint_rtol=1.0e-3)
-            self.x = temp_x[1, :, :]
-            self.u = inputvector_output[:, :, t]
-            output[:, :, t] = self.output(timevector[t], self.x)
         return output
 
     def nfe(self):
